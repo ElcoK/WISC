d9a9a89afc570e2483161483f9819dd7966c6d27
elco.k <elcokoks@gmail.com>
2018-06-07 16:46:11 +0200

    get docs working

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # add list of modules used
 import mock
 
-MOCK_MODULES = ['numpy', 'json','shutil','pandas', 'geopandas', 'rasterio','rasterstats','multiprocess','subprocess','prepare','utils']
+MOCK_MODULES = ['numpy','json','shutil','pandas', 'geopandas','rasterstats','multiprocess','subprocess','prepare','utils']
 				
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
--- scripts/utils.py
+++ scripts/utils.py
@@ -76,15 +76,11 @@ def int2date(argdate: int):
     """
     If you have date as an integer, use this method to obtain a datetime.date object.
 
-    Parameters
-    ----------
-    argdate : int
-      Date as a regular integer value (example: 20160618)
+    Arguments:
+    argdate {int} -- Date as a regular integer value (example: 20160618)
 
-    Returns
-    -------
-    dateandtime.date
-      A date object which corresponds to the given value `argdate`.
+    Returns:
+    dateandtime.date -- A date object which corresponds to the given value `argdate`.
     """
     year = int(argdate / 10000)
     month = int((argdate % 10000) / 100)
